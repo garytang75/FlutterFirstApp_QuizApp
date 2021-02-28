ef44050b3e59993648f11fce39c048c01f0379ba
Zifeng <gcorbett1218@gmail.com>
2021-02-28 17:17:21 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 5},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answer': [
+        {'text': 'Rabbit', 'score': 2},
+        {'text': 'Snake', 'score': 2},
+        {'text': 'Elephant', 'score': 2},
+        {'text': 'Lion', 'score': 2}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite food',
+      'answer': [
+        {'text': 'Burger', 'score': 2},
+        {'text': 'Donut', 'score': 2},
+        {'text': 'Coffee', 'score': 2},
+        {'text': 'Pizza', 'score': 2}
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
